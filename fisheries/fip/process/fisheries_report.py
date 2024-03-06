bf027a7b89e5566dc74aef691189bee008d01cca
Ian Edwards <ian@myacorn.com>
2024-03-06 21:13:54 +0000

    Add vars to template

--- fisheries/fip/process/fisheries_report.py
+++ fisheries/fip/process/fisheries_report.py
@@ -18,27 +18,27 @@ output = """
     <tr><th class="col-1"><b>Total</b></td><td></td><td></td></tr>
 
     <tr><th colspan=3 class="col-1"><b>Species</b></td></tr>
-    <tr><td>Red Snapper</td><td></td><td></td></tr>
-    <tr><td>Mid-depth snappers</td><td></td><td></td></tr>
-    <tr><td>Shallow-water snappers</td><td></td><td></td></tr>
-    <tr><td>Shallow-water groupers</td><td></td><td></td></tr>
-    <tr><td>Deep-water groupers</td><td></td><td></td></tr>
-    <tr><td>Tilefishes</td><td></td><td></td></tr>
-    <tr><td>Jacks</td><td></td><td></td></tr>
-    <tr><td>Triggerfishes</td><td></td><td></td></tr>
-    <tr><td>Grunts and porgies</td><td></td><td></td></tr>
-    <tr><td>Coastal pelagic</td><td></td><td></td></tr>
+    <tr><td>Red Snapper</td><td>{RF10_land_e_RS}</td><td>{RF10_rev_e_RS}</td></tr>
+    <tr><td>Mid-depth snappers</td><td>{RF10_land_e_MS}</td><td>{RF10_rev_e_MS}</td></tr>
+    <tr><td>Shallow-water snappers</td><td>{RF10_land_e_SS}</td><td>{RF10_rev_e_SS}</td></tr>
+    <tr><td>Shallow-water groupers</td><td>{RF10_land_e_SG}</td><td>{RF10_rev_e_SG}</td></tr>
+    <tr><td>Deep-water groupers</td><td>{RF10_land_e_DG}</td><td>{RF10_rev_e_DG}</td></tr>
+    <tr><td>Tilefishes</td><td>{RF10_land_e_TF}</td><td>{RF10_rev_e_TF}</td></tr>
+    <tr><td>Jacks</td><td>{RF10_land_e_JA}</td><td>{RF10_rev_e_JA}</td></tr>
+    <tr><td>Triggerfishes</td><td>{RF10_land_e_TR}</td><td>{RF10_rev_e_TR}</td></tr>
+    <tr><td>Grunts and porgies</td><td>{RF10_land_e_GP}</td><td>{RF10_rev_e_GP}</td></tr>
+    <tr><td>Coastal pelagic</td><td>{RF10_land_e_CP}</td><td>{RF10_rev_e_CP}</td></tr>
 
     <tr><th colspan=3 class="col-1"><b>Time period</b></td></tr>
-    <tr><td>2007-2014</td><td></td><td></td></tr>
-    <tr><td>2015-2021</td><td></td><td></td></tr>
+    <tr><td>2007-2014</td><td>{RF10_land_t_2007_2014}</td><td>{RF10_rev_t_2007_2014}</td></tr>
+    <tr><td>2015-2021</td><td>{RF10_land_t_2015_2021}</td><td>{RF10_rev_t_2015_2021}</td></tr>
 
     <tr><th colspan=3 class="col-1"><b>State</b></td></tr>
-    <tr><td>Florida</td><td></td><td></td></tr>
-    <tr><td>Alabama</td><td></td><td></td></tr>
-    <tr><td>Mississippi</td><td></td><td></td></tr>
-    <tr><td>Louisiana</td><td></td><td></td></tr>
-    <tr><td>Texas</td><td></td><td></td></tr>
+    <tr><td>Florida</td><td>{RF10_land_s_FL}</td><td>{RF10_rev_s_FL}</td></tr>
+    <tr><td>Alabama</td><td>{RF10_land_s_AL}</td><td>{RF10_rev_s_AL}</td></tr>
+    <tr><td>Mississippi</td><td>{RF10_land_s_MS}</td><td>{RF10_rev_s_MS}</td></tr>
+    <tr><td>Louisiana</td><td>{RF10_land_s_LA}</td><td>{RF10_rev_s_LA}</td></tr>
+    <tr><td>Texas</td><td>{RF10_land_s_TX}</td><td>{RF10_rev_s_TX}</td></tr>
   </tbody>
 </table>
 
@@ -52,10 +52,10 @@ output = """
 
 """
 
-
+''' unused:
 OrderedDict([
     ('Red Snapper', ['RF10_land_e_RS', 'RF10_rev_e_RS']),
-    ('Mid-depth snappers', ['RF10_land_e_MS.R', 'RF10_rev_e_MS']),
+    ('Mid-depth snappers', ['RF10_land_e_MS', 'RF10_rev_e_MS']),
     ('Shallow-water snappers', ['RF10_land_e_SS', 'RF10_rev_e_SS']),
     ('Shallow-water groupers', ['RF10_land_e_SG', 'RF10_rev_e_SG']),
     ('Deep-water groupers', ['RF10_land_e_DG', 'RF10_rev_e_DG']),
@@ -72,7 +72,45 @@ OrderedDict([
     ('Louisiana', ['RF10_land_s_LA', 'RF10_rev_s_LA']),
     ('Texas', ['RF10_land_s_TX', 'RF10_rev_s_TX'])
 ])
+'''
 
+values = {
+    'RF10_land_e_RS': "",
+    'RF10_rev_e_RS': "",
+    'RF10_land_e_MS': "",
+    'RF10_rev_e_MS': "",
+    'RF10_land_e_SS': "",
+    'RF10_rev_e_SS': "",
+    'RF10_land_e_SG': "",
+    'RF10_rev_e_SG': "",
+    'RF10_land_e_DG': "",
+    'RF10_rev_e_DG': "",
+    'RF10_land_e_TF': "",
+    'RF10_rev_e_TF': "",
+    'RF10_land_e_JA': "",
+    'RF10_rev_e_JA': "",
+    'RF10_land_e_TR': "",
+    'RF10_rev_e_TR': "",
+    'RF10_land_e_GP': "",
+    'RF10_rev_e_GP': "",
+    'RF10_land_e_CP': "",
+    'RF10_rev_e_CP': "",
+    '2007-2014': "",
+    'RF10_land_t_2007_2014': "",
+    'RF10_rev_t_2007_2014': "",
+    'RF10_land_t_2015_2021': "",
+    'RF10_rev_t_2015_2021': "",
+    'RF10_land_s_FL': "",
+    'RF10_rev_s_FL': "",
+    'RF10_land_s_AL': "",
+    'RF10_rev_s_AL': "",
+    'RF10_land_s_MS': "",
+    'RF10_rev_s_MS': "",
+    'RF10_land_s_LA': "",
+    'RF10_rev_s_LA': "",
+    'RF10_land_s_TX': "",
+    'RF10_rev_s_TX': "-",
+}
 
 LOGGER = logging.getLogger(__name__)
 
@@ -157,6 +195,7 @@ class FisheriesReportProcessor(BaseProcessor):
             'Report': output.format(
                 name="",
                 comments=value,
+                **values,
             )
         }
 
